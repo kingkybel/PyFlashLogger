aa46a41bc186cd84d1788107434a67bac839e7e2
Dieter J Kybelksties <github@kybelksties.com>
2026-03-05 14:57:47 +0000

    auto-committed by release-to-pypi.py

--- flashlogger/__init__.py
+++ flashlogger/__init__.py
@@ -1,8 +1,8 @@
-from .flash_logger import *
-from .log_channel_file import *
-from .log_channel_abc import *
-from .log_levels import *
 from .color_scheme import *
+from .log_channel_abc import *
+from .log_channel_file import *
 from .log_channel_console import *
+from .log_levels import *
+from .flash_logger import *
 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
